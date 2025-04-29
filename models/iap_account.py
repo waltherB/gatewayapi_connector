fc5ddffa1faaab527503e593fe9f4e9b3b891f09
Walther Barnett <>
2025-04-29 10:21:56 +0200

    Feat: Allow user to configure Sender Name for GatewayAPI in IAP Account form

--- models/iap_account.py
+++ models/iap_account.py
@@ -13,6 +13,14 @@ class IapAccount(models.Model):
         ('global', 'Global Platform (gatewayapi.com)')
     ], string='GatewayAPI Platform', default='eu')
     gatewayapi_token = fields.Char(string='API Token')
+    gatewayapi_sender = fields.Char(
+        string='Sender Name',
+        help=(
+            'Sender name for SMS sent via GatewayAPI. '
+            'Alphanumeric up to 11 chars or numeric up to 15 digits.'
+        ),
+        size=15
+    )
 
     @api.model
     def _get_service_from_provider(self, provider_type):
