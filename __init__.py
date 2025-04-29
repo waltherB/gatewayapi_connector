7d9637e7f7bb49eb4fb6065d4b7bedf78124ae21
Walther Barnett <>
2025-04-29 12:26:53 +0200

    Fix: Remove invalid field definition from __init__.py (field must be in model class only)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,2 @@
 from . import models
 from . import controllers
-
-gatewayapi_base_url = fields.Selection([
-    ('eu', 'EU Platform (gatewayapi.eu)'),
-    ('global', 'Global Platform (gatewayapi.com)')
-], string='GatewayAPI Platform', default='eu', required=True)
